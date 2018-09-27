8c0db73414bc63b6a44c0c7141cad4253ebee510
Ikechi Michael <mykehell123@gmail.com>
2018-09-27 01:44:17 +0100

    chore: update version to 1.0.1

--- src/spectranet-cli.js
+++ src/spectranet-cli.js
@@ -9,7 +9,7 @@ const program = require('commander')
 // define sub commands
 
 program
-    .version('0.0.1')
+    .version('1.0.1')
     .command('info [env]', 'displays basic information', { isDefault: true })
     .command('payments [env]', 'displays payment history')
     .command('usage [env]', 'displays usage histories')
