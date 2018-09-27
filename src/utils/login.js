2d127ff381e744aebe45c3fd9e026fb8428010a1
Ikechi Michael <mykehell123@gmail.com>
2018-09-27 01:43:35 +0100

    feat: ask for username and password if not provided in .env

--- src/utils/login.js
+++ src/utils/login.js
@@ -1,11 +1,21 @@
 const dotenv = require('dotenv')
+const readline = require('readline-sync')
 
 dotenv.config()
 
-const username = process.env.SPECTRANET_USERNAME;
-const password = process.env.SPECTRANET_PASSWORD;
-
 const login = async (page) => {
+    let username = process.env.SPECTRANET_USERNAME;
+    let password = process.env.SPECTRANET_PASSWORD;
+
+    if (!username) {
+        username = readline.question('Enter Username: ')
+    }
+
+    if (!password) {
+        password = readline.question('Enter Password: ', { hideEchoBack: true })
+        console.log('-------------------------')
+    }
+    
     await page.evaluate((username, password) => {
         $('[name="signInForm.username"]').val(username)
         $('[name="signInForm.password"]').val(password)
