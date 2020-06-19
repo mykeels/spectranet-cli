75b3e5f57155c781a2abb06d178fc121c7985f08
Justice Otuya <jotuya2@gmail.com>
2020-06-19 17:58:01 +0100

    show actual data balance for user account (#6)
    Co-authored-by: Justice  Otuya <justice.otuya@digitalpulse.ng>

--- src/utils/get-info.js
+++ src/utils/get-info.js
@@ -5,6 +5,13 @@ const getInfo = async (page) => {
         })
     })
 
+    const bundleItems = await page.evaluate(() => {
+        return $('.dataTable tr td').map(function () {
+            return $(this).text()
+        })
+    })
+
+
     const data = {
         loginId: items[0],
         accountId: items[1],
@@ -16,14 +23,15 @@ const getInfo = async (page) => {
         currentDataPlan: {
             name: items[7],
             description: items[8]
-        }
+        },
+        remainingBundle: bundleItems
     }
 
     return data
 }
 
-const print = ({ loginId, accountId,  lastLoginDate, dataExpirationDate,  accountBalance, 
-                lastPaymentDate,  nextRenewalDate, currentDataPlan }) => {
+const print = ({ loginId, accountId, lastLoginDate, dataExpirationDate, accountBalance,
+    lastPaymentDate, nextRenewalDate, currentDataPlan, remainingBundle }) => {
     if (loginId) console.log('Login ID: ', loginId)
     if (accountId) console.log('Account ID: ', accountId)
     if (lastLoginDate) console.log('Last Seen: ', lastLoginDate)
@@ -34,6 +42,11 @@ const print = ({ loginId, accountId,  lastLoginDate, dataExpirationDate,  accoun
     if (currentDataPlan) {
         if (currentDataPlan.name) console.log('Current Data Plan: ', currentDataPlan.name)
         if (currentDataPlan.description) console.log('\t\t : ', currentDataPlan.description)
+    }
+    if (remainingBundle) {
+        console.log(`${remainingBundle[0]}:  ${remainingBundle[1]}`)
+        console.log(`${remainingBundle[2]}:  ${remainingBundle[3]}`)
+        console.log(`${remainingBundle[4]}:  ${remainingBundle[5]}`)
     }
 }
 
